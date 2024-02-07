63572d5e3aba67630f07e3f389bd6b4d26960970
pra1405 <141804228+pra1405@users.noreply.github.com>
2024-02-07 21:24:59 +0530

    Update chat.py

--- sdk/python/foundation-models/maas/llama2/LangChain/chat.py
+++ sdk/python/foundation-models/maas/llama2/LangChain/chat.py
@@ -7,11 +7,11 @@ from langchain_community.chat_models.azureml_endpoint import (
     LlamaChatContentFormatter,
 )
 config = dotenv_values("key1.env")
-dict(config)
+config1=dict(config)
 chat = AzureMLChatOnlineEndpoint(
-    endpoint_url=config["KEY"],
+    endpoint_url=config1["KEY"],
     endpoint_api_type=AzureMLEndpointApiType.serverless,
-    endpoint_api_key=config["BASE_URL"],
+    endpoint_api_key=config1["BASE_URL"],
     content_formatter=LlamaChatContentFormatter(),
     model_kwargs={"temperature": 0.8},
 )
