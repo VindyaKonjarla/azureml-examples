5fe29e3cc03fcdfaaeae18fb03542cfde9760e16
pra1405 <141804228+pra1405@users.noreply.github.com>
2024-02-07 21:24:15 +0530

    Update chat completions.py

--- sdk/python/foundation-models/maas/llama2/LangChain/chat completions.py
+++ sdk/python/foundation-models/maas/llama2/LangChain/chat completions.py
@@ -8,11 +8,11 @@ from langchain_community.chat_models.azureml_endpoint import (
     AzureMLOnlineEndpoint,
 )
 config = dotenv_values("key1.env")
-dict(config)
+config1=dict(config)
 chat = AzureMLOnlineEndpoint(
-    endpoint_url=config["KEY"],
+    endpoint_url=config1["API_KEY"],
     endpoint_api_type=AzureMLEndpointApiType.serverless,
-    endpoint_api_key=config["BASE_URL"],
+    endpoint_api_key=config1["BASE_URL"],
     content_formatter=LlamaChatContentFormatter(),
     model_kwargs={"temperature": 0.8, "max_new_tokens": 400},
 )
