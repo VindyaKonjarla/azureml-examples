ac286704a7646baab7c942a42560f57290b5cd40
pra1405 <141804228+pra1405@users.noreply.github.com>
2024-02-08 10:25:32 +0530

    Update chat completions.py

--- sdk/python/foundation-models/maas/llama2/LangChain/chat completions.py
+++ sdk/python/foundation-models/maas/llama2/LangChain/chat completions.py
@@ -7,14 +7,14 @@ from langchain_community.chat_models.azureml_endpoint import (
     LlamaChatContentFormatter,
     AzureMLOnlineEndpoint,
 )
-config = dotenv_values("key1.env")
-config1=dict(config)
+config = dotenv_values("key1.env") #Load the environmet variable from the .env file
+config1=dict(config)    # converting into key and values from the list
 chat = AzureMLOnlineEndpoint(
-    endpoint_url=config1["API_KEY"],
-    endpoint_api_type=AzureMLEndpointApiType.serverless,
-    endpoint_api_key=config1["BASE_URL"],
-    content_formatter=LlamaChatContentFormatter(),
-    model_kwargs={"temperature": 0.8, "max_new_tokens": 400},
+    endpoint_url=config1["BASE_URL"],  # Load the URL from the .env file
+    endpoint_api_type=AzureMLEndpointApiType.serverless,#It determines the communication protocol and format used to send and receive data between clients and the deployed model
+    endpoint_api_key=config1["API_KEY"], ## Load the primary/secondary key or AMLToken for the endpoint from the .env file
+    content_formatter=LlamaChatContentFormatter(),#The content_formatter parameter is a handler class for transforming the request and response of an AzureML endpoint to match with required schema
+    model_kwargs={"temperature": 0.8, "max_new_tokens": 400},#temperature value defines the accuracy of the output, max_new_tokens gives the no of words 
 )
-response = llm.invoke("My name is Julien and I like to")
+response = llm.invoke("My name is Julien and I like to")# fetching the output from endpoint
 response
